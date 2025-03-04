8b50ef29f2d0ba340dd040b6ea69321ea5104c8b
chompypotato <fletcher.hoch@outlook.com>
2025-03-04 07:20:50 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,31 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import serverless from 'serverless-http';
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const serverless = require('serverless-http');
 
 const app = express();
 
 app.use('/', createProxyMiddleware({
   target: 'https://shellshock.io',
   changeOrigin: true,
+  selfHandleResponse: true, // We'll manually handle the response
+  onProxyRes: (proxyRes, req, res) => {
+    let body = [];
+    proxyRes.on('data', chunk => {
+      body.push(chunk);
+    });
+    proxyRes.on('end', () => {
+      const buffer = Buffer.concat(body);
+      // Remove the transfer-encoding header if set
+      res.removeHeader('transfer-encoding');
+      res.setHeader('Content-Length', buffer.length);
+      res.end(buffer);
+    });
+  },
   onError: (err, req, res) => {
     console.error('Proxy error:', err);
     res.status(500).send('Proxy error: ' + err.message);
   }
 }));
 
-// Export the wrapped Express app as the default export.
-export default serverless(app);
+// Export the wrapped app as the default export for serverless-http.
+module.exports = { default: serverless(app) };
